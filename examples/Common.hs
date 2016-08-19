87d0a2a7664b6899b487527418d10f7fb6e67991
ziocroc <ziocroc@gmail.com>
2016-08-20 01:14:36 +0200

    Added JavaScript backend for Common.hs

--- examples/Common.hs
+++ examples/Common.hs
@@ -1,21 +1,100 @@
+{-# LANGUAGE CPP #-}
+
+#ifdef __GHCJS__
+{-# LANGUAGE OverloadedStrings, JavaScriptFFI, InterruptibleFFI #-}
+#endif
+
 module Common (
         animation,
         static,
         loadTexture
 ) where
 
-import Codec.Picture
-import Codec.Picture.Types (promoteImage)
-import Control.Concurrent
-import Control.Monad.IO.Class (liftIO)
-import Data.IORef
-import Data.Hashable
-import qualified Data.Vector.Storable as V
 import Graphics.Rendering.Ombra.Generic
 import qualified Graphics.Rendering.Ombra.D2 as D2
 import Graphics.Rendering.Ombra.Draw
+import Graphics.Rendering.Ombra.Texture
+import Control.Monad.IO.Class (liftIO)
+import Data.Hashable
+import Data.IORef
+
+#ifdef __GHCJS__
+
+import Data.String
+import Graphics.Rendering.Ombra.Backend.WebGL
+import GHCJS.Types
+import JavaScript.TypedArray
+import JavaScript.TypedArray.Internal
+import JavaScript.TypedArray.DataView
+
+foreign import javascript unsafe "document.querySelector($1)"
+        query :: JSString -> IO JSVal
+
+foreign import javascript interruptible
+        "var img = new Image();                                         \
+         img.src = $1;                                                  \
+         img.onload = function () {                                     \
+                 var canvas = document.createElement('canvas'),         \
+                     ctx = canvas.getContext('2d');                     \
+                 canvas.width = img.width;                              \
+                 canvas.height = img.height;                            \
+                 ctx.drawImage(img, 0, 0);                              \
+                 var imgData = ctx.getImageData( 0, 0, img.width        \
+                                               , img.height);           \
+                 $c({ data: new Uint8Array(imgData.data.buffer)         \
+                    , width: imgData.width, height: imgData.height });  \
+          };                                                            "
+        loadImage :: JSString -> IO JSVal
+
+foreign import javascript unsafe "$1.width" imgWidth :: JSVal -> IO Int
+foreign import javascript unsafe "$1.height" imgHeight :: JSVal -> IO Int
+foreign import javascript unsafe "$1.data" imgData :: JSVal -> IO Uint8Array
+
+foreign import javascript interruptible "window.requestAnimationFrame($c);"
+        waitFrame :: IO Double
+
+animation :: (Float -> Layer) -> IO ()
+animation layer = do canvas <- query "#canvas"
+                     ctx <- makeContext canvas
+                     stateRef <- drawState 512 512 >>= newIORef
+
+                     flip (refDrawCtx ctx) stateRef $
+                              do drawInit
+                                 loop $ \t -> do
+                                     drawBegin
+                                     drawLayer . layer $ realToFrac t
+                                     drawEnd
+
+                     return ()
+        where loop a = do t <- liftIO waitFrame
+                          a $ t / 1000
+                          loop a
+
+loadTexture :: FilePath -> IO Texture
+loadTexture path = do img <- loadImage $ fromString path
+                      w <- imgWidth img
+                      h <- imgHeight img
+                      arr <- imgData img
+
+                      let buf = buffer arr
+                          dw = dataView buf
+                          l = w * h * 4
+                          hash = flip hashWithSalt l $
+                                        foldl (\s i -> hashWithSalt s $
+                                                        getUint8 i dw)
+                                        0x36d1615b7400a4
+                                        [ 0 .. l - 1 ]
+
+                      print hash
+                      return $ mkTextureRaw w h arr hash
+
+#else
+
+import Codec.Picture
+import Codec.Picture.Types (promoteImage)
+import Control.Concurrent
+import qualified Data.Vector.Storable as V
 import Graphics.Rendering.Ombra.Backend.OpenGL
-import Graphics.Rendering.Ombra.Texture (mkTexture)
 import Graphics.UI.GLFW as G
 
 animation :: (Float -> Layer) -> IO ()
@@ -41,15 +120,18 @@ animation layer =
                             loop (n + fromIntegral delay / 1000000) a
               delay = 30000
 
-static :: Layer -> IO ()
-static = animation . const
-
 loadTexture :: FilePath -> IO Texture
 loadTexture path = do eimg <- readImage path
                       case eimg of
                            Left err -> error err
                            Right img ->
                                    case convertRGBA8 img of
+                                        {-
+                                        Image w h v ->
+                                            let (fp, l) = unsafeToForeignPtr0 v
+                                                hash = hash ...
+                                            in mkTextureRaw ...
+                                        -}
                                         Image w h v -> return . mkTexture w h $
                                                         colList v
         where colList = fst . V.foldr (\x (l, cs) ->
@@ -59,3 +141,7 @@ loadTexture path = do eimg <- readImage path
                                              _ -> (l, x : cs)
                                       )
                                       ([], [])
+#endif
+
+static :: Layer -> IO ()
+static = animation . const
