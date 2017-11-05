e9b2fdd28a9541bdc5796054b50aa55634a7ebaf
ziocroc <ziocroc@gmail.com>
2017-11-05 15:17:38 +0100

    Remove MonadGL constraint from MonadCulling

--- Graphics/Rendering/Ombra/Culling/Draw.hs
+++ Graphics/Rendering/Ombra/Culling/Draw.hs
@@ -5,5 +5,5 @@ module Graphics.Rendering.Ombra.Culling.Draw (
 import Graphics.Rendering.Ombra.Internal.GL
 import Graphics.Rendering.Ombra.Culling.Types
 
-class (GLES, MonadGL m) => MonadCulling m where
+class GLES => MonadCulling m where
         withCulling :: Maybe CullFace -> m a -> m a
